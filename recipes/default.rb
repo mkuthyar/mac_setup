407399d017a3ed8fca4bf9fbdbe4034b62e57c16
Mihir Kuthyar <mihirkuthyar@MacBook-Pro.local>
2020-05-31 18:10:45 -0400

    added execution instructions

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,5 +4,7 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+# To execute recipe in local mode, clone down the cookbook and run: chef-client -z -o 'recipe[mac_setup::default]'
+
 include_recipe 'mac_setup::bash_profile'
 include_recipe 'mac_setup::software_installs'
